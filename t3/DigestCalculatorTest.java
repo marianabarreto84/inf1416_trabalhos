8a0ffb5b445bdbb5f5993fb009d9c448a5b237d1
marianabarreto84 <77171403+marianabarreto84@users.noreply.github.com>
2023-04-19 18:32:57 -0300

    alterando classe de teste

--- t3/DigestCalculatorTest.java
+++ t3/DigestCalculatorTest.java
@@ -21,25 +21,8 @@ public class DigestCalculatorTest {
             // Calcular o digest dos arquivos na pasta temporária usando o algoritmo MD5
             String algorithm = "MD5";
             try {
-                MessageDigest digest = MessageDigest.getInstance(algorithm);
-                File[] files = directory.listFiles();
-                for (File file : files) {
-                    if (file.isFile()) {
-                        FileInputStream fis = new FileInputStream(file);
-                        byte[] buffer = new byte[1024];
-                        int n = 0;
-                        while ((n = fis.read(buffer)) != -1) {
-                            digest.update(buffer, 0, n);
-                        }
-                        fis.close();
-                        byte[] fileDigest = digest.digest();
-                        String fileDigestString = bytesToHex(fileDigest);
-                        System.out.println(file.getName() + ": " + fileDigestString);
-                    }
-                }
-            } catch (NoSuchAlgorithmException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
+                DigestCalculator.calculateDigest(directory, algorithm);
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         
@@ -51,18 +34,6 @@ public class DigestCalculatorTest {
         } catch (IOException e1) {
             e1.printStackTrace();
         }
-    }
-    
-    private static final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
-
-    private static String bytesToHex(byte[] bytes) {
-        char[] hexChars = new char[bytes.length * 2];
-        for (int i = 0; i < bytes.length; i++) {
-            int v = bytes[i] & 0xFF;
-            hexChars[i * 2] = HEX_ARRAY[v >>> 4];
-            hexChars[i * 2 + 1] = HEX_ARRAY[v & 0x0F];
-        }
-        return new String(hexChars);
     }
 
 }
